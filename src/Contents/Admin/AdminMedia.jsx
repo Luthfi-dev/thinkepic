1a5b9cb579978e240e3919005eeee6c43ba4366a
Luthfi-dev <luthfi2048@gmail.com>
2023-11-01 11:18:54 +0700

    addd loader di media

--- src/Contents/Admin/AdminMedia.jsx
+++ src/Contents/Admin/AdminMedia.jsx
@@ -7,6 +7,10 @@ import { showDynamicAlert } from '../showDynamicAlert';
 import { DataUser } from "@/components/DataUser";
 import { useRouter } from "next/router";
 
+const imageLoader = ({ src, width, quality }) => {
+  return `${src}?w=${width}&q=${quality || 75}`;
+};
+
 const AdminContent = ({kData, modal}) => {
   const [isFileSelected, setIsFileSelected] = useState(false);
   const [selectedFiles, setSelectedFiles] = useState([]);
@@ -220,7 +224,7 @@ const renderPagination = () => {
                     {dataAll.map((image) => (
                       <div key={image.id} className="col-md-3 mb-3">
                         <div className="d-flex flex-column align-items-center">
-                          <Image src={`${publicApi}/${image.nama}`} alt={image.id} width={200} height={200} objectFit="cover" onClick={() => handleMediaClick(image.nama)} />
+                          <Image src={`${publicApi}/${image.nama}`} alt={image.id} width={200} height={200} objectFit="cover" onClick={() => handleMediaClick(image.nama)} loader={imageLoader} />
                           <button onClick={() => deleteMedia(image.id)} className="btn btn-danger mt-2"><i className='bi bi-trash'></i></button>
                         </div>
                       </div>
